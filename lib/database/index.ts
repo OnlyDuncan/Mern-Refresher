7f1aea0efe7b524ef4217c55c5c0a52575523f09
Duncan Payne <Duncan.C.Payne@gmail.com>
2024-02-25 17:58:15 -0600

    Troubleshooting More

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -10,7 +10,6 @@ export const connectToDatabase = async () => {
     if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'soiree',
         bufferCommands: false,
     })
 
